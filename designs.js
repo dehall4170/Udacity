6dd825c852045a9d7a6b7431ee0b0b67c3434a94
dehall4170 <49341555+dehall4170@users.noreply.github.com>
2019-04-06 16:39:31 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,31 +1,33 @@
+// grid should appear when user enters desired width and height of the grid
 function makeGrid() {
-  let table = $("#pixelCanvas");
-  const colorPicker = $("#colorPicker");
-  let cols = $("#inputWidth").val();
-  let rows = $("#inputHeight").val();
-
-  table.children().remove();
-
-  for (let i = 0; i < rows; i++) {
-    table.append("<tr></tr>");
-
-    for (let j = 0; j < cols; j++)
-      table
-        .children()
-        .last()
-        .append("<td></td>");
-  }
-
-  table.on("click", "td", function() {
-    let color = $("input[type='color']#colorPicker").val();
-
-    $(this).attr("bgcolor", color);
-  });
+    let table = $("#pixelCanvas");
+    const colorPicker = $("#colorPicker");
+    // gets values for height and width from input and calls makeGrid
+    let cols = $("#inputWidth").val();
+    let rows = $("#inputHeight").val();
+    // grid is cleared or created when submit is clicked
+    table.children().remove();
+    // loop over each row
+    for (let i = 0; i < rows; i++) {
+        table.append("<tr></tr>");
+        // loop for each cell
+        for (let j = 0; j < cols; j++)
+            table
+            .children()
+            .last()
+            .append("<td></td>");
+    }
+    // color picker color is applied on the clicked cell
+    table.on("click", "td", function() {
+        let color = $("input[type='color']#colorPicker").val();
+        // changes color of pixel when clicked
+        $(this).css('background-color', color);
+    });
 }
 
 const submitButton = $("input[type='submit']");
-
+// builds selected grid
 submitButton.click(function(e) {
-  e.preventDefault();
-  makeGrid();
+    e.preventDefault();
+    makeGrid();
 });
